5a097b73ff7fea9dc739013e7321d86fd7e7c153
Dima_notebook <kovyazinaa11@ya.ru>
2021-06-01 00:02:37 +0300

    add AboutMe.jsx

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './AboutMe.css'
+import photo from '../../images/me.jpg'
 
 export default function AboutMe() {
   return (
-    <section>
-      <h1>привет</h1>
+    <section className="about-me">
+      <h1 className="about-me__title">Студент</h1>
+      <div className="about-me__container">
+        <div className="about-me__info">
+          <p className="about-me__info_name">Дмитрий</p>
+          <p className="about-me__info_profession">Фронтенд-разработчик, 27 лет</p>
+          <p className="about-me__info_activity">Я родился в г.Южно-Сахалинск, проживал в Крыму и переехал в г.Краснодар. Учился на юриста в Академии Ген. Прокуратуры РФ, но на 3 курсе по ряду причин ушел работать в ООО "Севтелеком". В 2018 г. ушел на службу в 
+          ВС ФНГ РФ. Я люблю слушать музыку, а ещё увлекаюсь её написанием. Недавно начал кодить. С 2019 года начал обучаться в «Яндекс Практикуме» во время службы, очень сильно увлекся и решил продолжить обучение на платной основе. После 
+того, как пройду курс по веб-разработке, надеюсь смогу обучаться еще более углубленно на реальных заказах в реальных проектах с будущими коллегами.</p>
+          <div className="about-me__info_links">
+            <a className="about-me__info_link" href="http://localhost:3000">VK</a>
+            <a className="about-me__info_link" href="http://localhost:3000">Github</a>
+          </div>
+        </div>
+        <img className="about-me__photo" src={photo} alt="Фото разработчика сайта"></img>
+      </div>
     </section>
   )
 }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ import Promo from '../Promo/Promo';
 import NavTab from '../NavTab/NavTab';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
-import AboutMe from './AboutMe/AboutMe';
+import AboutMe from '../AboutMe/AboutMe';
 
 function App() {
   return (
