317ee773220770a81a76a13aca34624ad48e650a
Dima Kovyazin <kovyazinaa11@yandex.ru>
2021-06-28 21:30:11 +0300

    add target=_blank && rel= noreferrer

--- src/components/FooterPage/FooterPage.jsx
+++ src/components/FooterPage/FooterPage.jsx
@@ -8,9 +8,9 @@ export default function FooterPage() {
       <div className="footer-page__container">
         <p className="footer-page__container_mark">&copy; 2021</p>
         <div className="footer-page__container_links">
-          <a className="footer-page__container_link" href="https://praktikum.yandex.ru/">Яндекс.Практикум</a>
-          <a className="footer-page__container_link" href="https://github.com/qwewerqwewer1">Github</a>
-          <a className="footer-page__container_link" href="https://vk.com/qwewerqwewer">VK</a>
+          <a className="footer-page__container_link" target="_blank" rel="noreferrer" href="https://praktikum.yandex.ru/">Яндекс.Практикум</a>
+          <a className="footer-page__container_link" target="_blank" rel="noreferrer" href="https://github.com/qwewerqwewer1">Github</a>
+          <a className="footer-page__container_link" target="_blank" rel="noreferrer" href="https://vk.com/qwewerqwewer">VK</a>
         </div>
       </div>
     </section>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -8,13 +8,13 @@ export default function Portfolio() {
     <>
       <p className="about-me__portfolio">Портфолио</p>
       <div className="about-me__portfolio_links">
-        <a href="https://github.com/qwewerqwewer1/how-to-learn" className="about-me__portfolio_link">Статичный сайт
+        <a href="https://github.com/qwewerqwewer1/how-to-learn" target="_blank" rel="noreferrer" className="about-me__portfolio_link">Статичный сайт
           <img className="about-me__portfolio_link_arrow" src={portfolio__arrow} alt="стрелка-ссылка"/>
         </a>
-        <a href="https://qwewerqwewer1.github.io/russian-travel/" className="about-me__portfolio_link">Адаптивный сайт
+        <a href="https://qwewerqwewer1.github.io/russian-travel/" target="_blank" rel="noreferrer" className="about-me__portfolio_link">Адаптивный сайт
           <img className="about-me__portfolio_link_arrow" src={portfolio__arrow} alt="стрелка-ссылка"/>
         </a>
-        <a href="http://black-box.nomoredomains.monster/" className="about-me__portfolio_link none-underline">Одностраничное приложение
+        <a href="http://black-box.nomoredomains.monster/" target="_blank" rel="noreferrer" className="about-me__portfolio_link none-underline">Одностраничное приложение
           <img className="about-me__portfolio_link_arrow" src={portfolio__arrow} alt="стрелка-ссылка"/>
         </a>
       </div>
